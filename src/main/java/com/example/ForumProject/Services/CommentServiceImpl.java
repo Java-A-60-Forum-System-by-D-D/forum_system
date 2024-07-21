c4eceb5422b5cdd20d19ed09ab2f0968c6098a20
DesZafirova <deszafirova@icloud.com>
2024-07-21 17:51:40 +0300

    implemented methods, I am considering creating AuthenticationHelper.class for authenticationValidator

--- src/main/java/com/example/ForumProject/Services/CommentServiceImpl.java
+++ src/main/java/com/example/ForumProject/Services/CommentServiceImpl.java
@@ -1,9 +1,58 @@
 package com.example.ForumProject.Services;
 
-import com.example.ForumProject.models.Comment;
+import com.example.ForumProject.exceptions.AuthorizationException;
+import com.example.ForumProject.models.*;
+import com.example.ForumProject.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
+@Service
 public class CommentServiceImpl implements CommentService {
-    List<Comment> getComments;
+    private final CommentRepository commentRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public List<Comment> getComments() {
+        return commentRepository.getComments();
+    }
+
+    @Override
+    public Comment getCommentById(int id) {
+        return commentRepository.getCommentById(id);
+    }
+
+    @Override
+    public Comment createComment(Comment comment) {
+        return commentRepository.createComment(comment);
+    }
+
+    @Override
+    public Comment updateComment(Comment comment, User user) {
+        authenticationValidator(user, comment);
+        return null;
+    }
+
+    @Override
+    public void deleteComment(int id, User user) {
+        authenticationValidator(user, commentRepository.getCommentById(id));
+        commentRepository.deleteComment(id);
+
+    }
+    private static void authenticationValidator(User user, Comment existingComment) {
+
+        Set<UserRole> userRoleSet = user.getUserRole();
+        UserRole userRoleAdmin = new UserRole(UserRoleEnum.ADMIN);
+
+        if (!userRoleSet
+                .contains(userRoleAdmin) && !Objects.equals(existingComment.getUser()
+                .getUsername(), user.getUsername())) {
+            throw new AuthorizationException("You have no permission to perform this action");
+        }
+    }
 }
