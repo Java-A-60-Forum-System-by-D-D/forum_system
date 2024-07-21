7cf23513750458746349400398e447572b58c5be
DesZafirova <deszafirova@icloud.com>
2024-07-21 17:48:58 +0300

    implement methods for comments

--- src/main/java/com/example/ForumProject/repositories/CommentRepository.java
+++ src/main/java/com/example/ForumProject/repositories/CommentRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface CommentRepository {
     List<Comment> getComments();
     Comment getCommentById(int id);
-    Comment addComment(Comment comment);
+    Comment createComment(Comment comment);
     Comment updateComment(Comment comment);
     void deleteComment(int id);
 
--- src/main/java/com/example/ForumProject/repositories/CommentRepositoryImpl.java
+++ src/main/java/com/example/ForumProject/repositories/CommentRepositoryImpl.java
@@ -1,32 +1,81 @@
 package com.example.ForumProject.repositories;
 
+import com.example.ForumProject.exceptions.EntityNotFoundException;
 import com.example.ForumProject.models.Comment;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
+@Repository
 public class CommentRepositoryImpl implements CommentRepository {
+    private final SessionFactory sessionFactory;
+
+    public CommentRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public List<Comment> getComments() {
-        return List.of();
+        try(Session session = sessionFactory.openSession()) {
+            Query<Comment> query = session.createQuery("from Comment ", Comment.class);
+            return query.list();
+        }
     }
 
     @Override
     public Comment getCommentById(int id) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            Query<Comment> query = session.createQuery("from Comment where id = :id", Comment.class);
+            query.setParameter("id", id);
+            if(query.list().isEmpty()){
+                throw new EntityNotFoundException("Comment", id);
+            }
+            return query.list().get(0);
+        }
     }
 
     @Override
-    public Comment addComment(Comment comment) {
-        return null;
+    public Comment createComment(Comment comment) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            comment.setCreatedAt(LocalDateTime.now());
+            comment.setUpdatedAt(LocalDateTime.now());
+            session.persist(comment);
+            session.getTransaction().commit();
+            return comment;
+        }
+
     }
 
     @Override
     public Comment updateComment(Comment comment) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            comment.setUpdatedAt(LocalDateTime.now());
+            session.merge(comment);
+            session.getTransaction().commit();
+            return comment;
+        }
+
     }
 
     @Override
     public void deleteComment(int id) {
+        try(Session session = sessionFactory.openSession()) {
+            Query<Comment> query = session.createQuery("from Comment where id = :id", Comment.class);
+            query.setParameter("id", id);
+            if(query.list().isEmpty()){
+                throw new EntityNotFoundException("Comment", id);
+            }
+            Comment comment = query.list().get(0);
+            session.beginTransaction();
+            session.remove(comment);
+            session.getTransaction().commit();
+
+        }
 
     }
 }
