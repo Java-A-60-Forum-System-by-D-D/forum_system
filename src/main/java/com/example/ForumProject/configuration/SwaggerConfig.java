e697f6a8fde5883f1e6adda39b4de6400f263436
DesZafirova <deszafirova@icloud.com>
2024-07-23 16:15:13 +0300

    fix some configuration, trying to hide some entities

--- src/main/java/com/example/ForumProject/configuration/SwaggerConfig.java
+++ src/main/java/com/example/ForumProject/configuration/SwaggerConfig.java
@@ -4,11 +4,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Configuration
@@ -42,5 +49,5 @@ public class SwaggerConfig {
                 .build();
     }
 
-
 }
+
